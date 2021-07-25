beec37445cda8edb4a3e73ae306fdbb1d889cc51
uliana fomina <6758599@gmail.com>
2021-07-25 12:27:37 +0300

    add main page

--- src/main/webapp/WEB-INF/jsp/main.js
+++ src/main/webapp/WEB-INF/jsp/main.js
@@ -8,3 +8,22 @@ for(i=0;i<acc.length; i++){
     this.nextElementSibling.classList.toggle("show");
   }
 }
+
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
